d60dc5c338096c946854658dc2e44050e35dae74
SYYYanyangyu <1245938403@qq.com>
2023-12-11 19:38:56 +0800

    同步后台添加字段

--- src/api/listenadmin/upload/index.ts
+++ src/api/listenadmin/upload/index.ts
@@ -17,9 +17,16 @@ enum API {
 export const reqFileExists = (model: FileExistsRequest) =>
   request.get<any, FileExistsResponse>(`${API.FileExists_URL}?fileSize=${model.fileSize}&sha256Hash=${model.sha256Hash}`)
 
-// 文件不存在上传文件
+// 上传音频
 export const UploadAudio = async (model: FormData): Promise<string> =>
   request.post<any, string>(API.Upload_URL, model, {
     headers: { "Content-Type": "multipart/form-data" },
   });
+
+// 上传图片
+export const UploadImages = async (model: FormData): Promise<string> => 
+  request.post<any,string>(API.UploadImages_URL,model, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
 
--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -1,13 +1,62 @@
 <template>
-    <div>
-      分页器组件
-    </div>
+  <div class="pagination">
+    <el-pagination
+      v-model:currentPage="currentPage"
+      :page-sizes="[10, 20, 30, 50]"
+      :page-size="pageSize"
+      layout="sizes, prev, pager, next, jumper, total"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { ElPagination } from 'element-plus';
 
+export default defineComponent({
+  components: {
+    ElPagination,
+  },
+  props: {
+    total: {
+      type: Number,
+      required: true,
+    },
+    pageSize: {
+      type: Number,
+      default: 10,
+    },
+  },
+  setup(props) {
+    const currentPage = ref(1);
+
+    function handleSizeChange(size: number) {
+      // 处理每页显示条数变化
+      console.log('每页显示条数：', size);
+    }
+
+    function handleCurrentChange(newPage: number) {
+      // 处理当前页码变化
+      console.log('当前页码：', newPage);
+      currentPage.value = newPage;
+    }
+
+    return {
+      currentPage,
+      handleSizeChange,
+      handleCurrentChange,
+    };
+  },
+});
 </script>
 
 <style scoped>
-
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
 </style>
